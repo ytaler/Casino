044f1342a9e697cff1cf6e9dcacc52cc9878609f
Yamil Taler <yamiltaler@gmail.com>
2018-08-26 23:11:08 -0300

    Adaptacion de codigo a PIC 18F45k20

---
+++ shift_io.c
@@ -0,0 +1,70 @@
+//=============================================================================
+// Funciones para leer y escribir datos serie a través de los registros
+// CD 4014 (8 entradas) y CD4094 (8 salidas)
+// Es necesario poner los TRIS al valor adecuado
+// Es necesario definir varios datos en "shifth_io.h"
+
+#include <stdint.h>        /* For uint8_t definition */
+#include "shift_io.h"
+#include "mcc_generated_files/mcc.h"
+
+//=============================================================================
+// Inicia los pines para CD4014
+void InitCD4014 (void)
+{
+   CLK_CD4014_SetLow();  // CLOCK inactivo
+   LOAD_CD4014_SetLow(); // LOAD inactivo entradas
+}
+
+//=============================================================================
+// Lee datos serie de los registros CD4014 al array global DatosCD4014[]
+void ReadCD4014 (void)
+{
+uint8_t bytes, data;
+
+//#ifdef PinTestCD4014
+   //output_high (PinTestCD4014); // depuracion
+//#endif
+
+// los pulsos son de 200ns a 20Mhz, poner retardos para alargarlos si es necesario
+// Primero carga TODOS los datos en paralelo en los registros
+   CLK_CD4014_SetHigh();    // LOAD activo
+   CLK_CD4014_SetHigh();     // CLK activo, carga los datos en paralelo
+   CLK_CD4014_SetLow();     // CLK inactivo
+   LOAD_CD4014_SetLow();     // LOAD inactivo
+
+// Ahora vamos leyendo en serie en grupos de 8 bits N veces
+   for (bytes = 0; bytes < NumeroCD4014; bytes++)
+   {
+// Forma larga pero rapida: 3:42us 2:29us 1:16us Aprox(3 us + 13*bytes us)
+      data = 0;
+      if (DATA_CD4014_GetValue()) data|=0x80;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      if (DATA_CD4014_GetValue()) data|=0x40;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      if (DATA_CD4014_GetValue()) data|=0x20;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      if (DATA_CD4014_GetValue()) data|=0x10;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      if (DATA_CD4014_GetValue()) data|=0x08;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      if (DATA_CD4014_GetValue()) data|=0x04;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      if (DATA_CD4014_GetValue()) data|=0x02;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      if (DATA_CD4014_GetValue()) data|=0x01;
+      CLK_CD4014_SetHigh(); // CLK activo, desplaza
+      CLK_CD4014_SetLow(); // CLK inactivo
+      DatosCD4014[bytes] = data; // Copiamos el dato compuesto al array
+   }
+//#ifdef PinTestCD4014
+   //output_low (PinTestCD4014); // depuracion
+//#endif
+}
---
+++ shift_io.h
@@ -0,0 +1,27 @@
+//=============================================================================
+// Funciones para leer y escribir datos serie a través de los registros
+// CD 4014 (8 entradas)
+// Es necesario poner los TRIS al valor adecuado
+// Es necesario definir:
+
+// #define PinTestCD4014            // Pin para pruebas de tiempo de ciclo
+//                                  // no se usa si no esta definido
+// #define CLK_CD4014               // Alias del pin LOAD para CD4014
+// #define DATA_CD4014              // Alias del pin DATA para CD4014
+// #define LOAD_CD4014              // Alias del pin CLK para CD4014
+// #define NumeroCD4014             // Numero de CD4014 en serie
+// int8 DatosCD4014[NumeroCD4014];  // Array de datos a leer de los CD4014
+// #define FastCD4014               // definirlo para usar el algoritmo rapido pero largo
+
+// #define PinTestCD4014  PIN_C4
+
+#define NumeroCD4014 4             // Numero de CD4014 en serie
+uint8_t DatosCD4014[NumeroCD4014];    // Array de datos a leer de los CD4014
+
+// Prototipos de las funciones:
+//=============================================================================
+// Inicia los pines para CD4014 a estado inactivo, llamarlo en la inicialización en
+// Main despues de haber puesto los TRIS a los valores adecuados
+void InitCD4014 (void);
+// Lee datos serie de los registros CD4014 al array global DatosCD4014[]
+void ReadCD4014 (void);
