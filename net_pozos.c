cfb62656abd518337237e63d5c145aa4030860cd
Yamil Taler <yamiltaler@gmail.com>
2018-09-09 23:17:36 -0300

    versiones iniciales de header y prototipos de funciones relacionadas al proyecto

---
+++ net_pozos.c
@@ -0,0 +1,17 @@
+#include "net_pozos.h"
+
+uint8_t verificarBotones(uint8_t datos){
+    uint8_t contador = 0;
+    datos &= 0x0F;
+    if ((datos & 0x01) == 0x01)
+        contador++;
+    if ((datos & 0x02) == 0x02)
+        contador++;
+    if ((datos & 0x04) == 0x04)
+        contador++;
+    if ((datos & 0x08) == 0x08)
+        contador++;
+    if (contador > 1)
+        datos = 0;
+    return datos;
+}
