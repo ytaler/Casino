c8d2125ee330ade9f00902609f27cb367eec6902
Yamil Taler <yamiltaler@gmail.com>
2018-09-09 23:21:45 -0300

    se eliminó la comparacion por el mismo valor de la mascara binaria, no tiene sentido

--- net_pozos.c
+++ net_pozos.c
@@ -3,13 +3,13 @@
 uint8_t verificarBotones(uint8_t datos){
     uint8_t contador = 0;
     datos &= 0x0F;
-    if ((datos & 0x01) == 0x01)
+    if (datos & 0x01)
         contador++;
-    if ((datos & 0x02) == 0x02)
+    if (datos & 0x02)
         contador++;
-    if ((datos & 0x04) == 0x04)
+    if (datos & 0x04)
         contador++;
-    if ((datos & 0x08) == 0x08)
+    if (datos & 0x08)
         contador++;
     if (contador > 1)
         datos = 0;
