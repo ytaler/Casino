7e3030a99914185d7a2469ce52d671aedd81b64b
Yamil Taler <yamil.taler@naranja.com>
2019-02-03 23:51:04 -0300

    Agregado de macros para RD7

--- mcc_generated_files/pin_manager.h
+++ mcc_generated_files/pin_manager.h
@@ -462,9 +462,22 @@
 #define IO_RD6_SetDigitalInput()    do { TRISDbits.TRISD6 = 1; } while(0)
 #define IO_RD6_SetDigitalOutput()   do { TRISDbits.TRISD6 = 0; } while(0)
 // PortD Bit6 = Salida RW (ReadWrite) para LCD
-// RW Se conectó directamente a cero, es decir en modo escritura
 #define	LCD_RW_SetHigh()            IO_RD6_SetHigh()
 #define	LCD_RW_SetLow()             IO_RD6_SetLow()
+
+// get/set IO_RD7 aliases
+#define IO_RD7_TRIS                 TRISDbits.TRISD7
+#define IO_RD7_LAT                  LATDbits.LATD7
+#define IO_RD7_PORT                 PORTDbits.RD7
+#define IO_RD7_SetHigh()            do { LATDbits.LATD7 = 1; } while(0)
+#define IO_RD7_SetLow()             do { LATDbits.LATD7 = 0; } while(0)
+#define IO_RD7_Toggle()             do { LATDbits.LATD7 = ~LATDbits.LATD7; } while(0)
+#define IO_RD7_GetValue()           PORTDbits.RD7
+#define IO_RD7_SetDigitalInput()    do { TRISDbits.TRISD7 = 1; } while(0)
+#define IO_RD7_SetDigitalOutput()   do { TRISDbits.TRISD7 = 0; } while(0)
+// PortD Bit7 = Salida control rele para VGND
+#define	SYSTEM_Power_On()           IO_RD7_SetHigh()
+#define	SYSTEM_Power_Off()          IO_RD7_SetLow()
 
 // ****************************************************************************
 // ********************               PORT E               ********************
